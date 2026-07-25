930ab458d6fb4a8cb2578427a765eaddc3be9044
danilo kuwai <danilorkuwai@gmail.com>
2026-07-25 14:38:10 -0300

    Implementando batida de ponto funcionario front

--- frontend/src/pages/PontoFuncionario/Ponto.jsx
+++ frontend/src/pages/PontoFuncionario/Ponto.jsx
@@ -1,5 +1,35 @@
+import '../PontoFuncionario/estilo/ponto.css'
+import {useState} from 'react';
+import api from '../../services/api';
+
 export default function PontoFuncionario(){
+
+    const[cpf,setCpf] = useState('');
+    const[senhaponto,setSenhaponto] = useState('');
+    
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+
+        try{
+            const resposta = await api.post('/ponto',{
+                cpf,
+                senha: senhaponto
+            });
+                console.log(resposta.data);
+        }catch(error){
+            console.error('Erro ao registrar o ponto:',error)
+        }
+
+    }
+
     return(
-        <div>Ponto Funcionário</div>
+        <div className="formulario" onSubmit={handleSubmit}>
+            <form>
+            <h2>Registro de Ponto</h2>
+            Digite seu CPF:<input type="text" value={cpf} onChange={e => setCpf(e.target.value)} required></input>
+            Digite sua Senha:<input type="password" value={senhaponto} onChange={e => setSenhaponto(e.target.value)} required></input>
+            <button type="submit">Enviar</button>
+            </form>
+        </div>
     )
 }
